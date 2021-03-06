14d0711f1b512de8c26ae274ebd427ea17331923
anandprakashdubey <dby.anandprakash@gmail.com>
2021-03-06 22:50:26 +0000

    Add project files.

---
+++ AsyncAPIDemo/Context/BookContext.cs
@@ -0,0 +1,56 @@
+﻿using AsyncAPIDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncAPIDemo.Context
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author()
+                {
+                    Id = Guid.Parse("47e57a40-323d-4253-a788-b96025013e50"),
+                    FirstName = "Agatha",
+                    LastName = "Kristie"
+                },
+                 new Author()
+                 {
+                     Id = Guid.Parse("a33a9a0f-98f8-45da-b4d2-08d95c406cb5"),
+                     FirstName = "Paulo",
+                     LastName = "Cohelo"
+                 }
+            );
+            modelBuilder.Entity<Book>().HasData(
+                new Book()
+                {
+                    Id = Guid.Parse("098a5bbf-4783-4c6d-8097-e0ff0f33c205"),
+                    AuthorId = Guid.Parse("47e57a40-323d-4253-a788-b96025013e50"),
+                    Title = "Murder of Rozer Ackward",
+                    Description = "Some Book",
+
+                },
+                 new Book()
+                 {
+                     Id = Guid.Parse("bd479151-9426-49c1-b827-267a076164a8"),
+                     AuthorId = Guid.Parse("a33a9a0f-98f8-45da-b4d2-08d95c406cb5"),
+                     Title = "Alchemist",
+                     Description = "Fiction Book",
+
+                 }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ AsyncAPIDemo/Migrations/20210306224138_InitialMigration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncAPIDemo.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(maxLength: 150, nullable: false),
+                    Description = table.Column<string>(maxLength: 2500, nullable: true),
+                    AuthorId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { new Guid("47e57a40-323d-4253-a788-b96025013e50"), "Agatha", "Kristie" });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { new Guid("a33a9a0f-98f8-45da-b4d2-08d95c406cb5"), "Paulo", "Cohelo" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Description", "Title" },
+                values: new object[] { new Guid("098a5bbf-4783-4c6d-8097-e0ff0f33c205"), new Guid("47e57a40-323d-4253-a788-b96025013e50"), "Some Book", "Murder of Rozer Ackward" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Description", "Title" },
+                values: new object[] { new Guid("bd479151-9426-49c1-b827-267a076164a8"), new Guid("a33a9a0f-98f8-45da-b4d2-08d95c406cb5"), "Fiction Book", "Alchemist" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
